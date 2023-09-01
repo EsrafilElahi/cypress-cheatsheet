050ca83bdb70d7b1b612c5f5f399fb20990d87ac
esrafilelahi <essinho1121@gmail.com>
2023-09-01 17:20:05 +0330

    adding...

--- cheatsheet/cy.api.cy.js
+++ cheatsheet/cy.api.cy.js
@@ -19,4 +19,15 @@ describe("API Test using plugin cypress-plugin-api", () => {
       expect(response.status).to.eq(204);
     });
   });
+  it('.as() - alias a route for later use', () => {
+    // Alias the route to wait for its response
+    cy.intercept('GET', '**/comments/*').as('getComment')
+
+    // we have code that gets a comment when
+    // the button is clicked in scripts.js
+    cy.get('.network-btn').click()
+
+    // https://on.cypress.io/wait
+    cy.wait('@getComment').its('response.statusCode').should('eq', 200)
+  })
 });
--- cheatsheet/cypress1.cy.js
+++ cheatsheet/cypress1.cy.js
@@ -221,3 +221,37 @@ cy.get("tbody th").each(function ($el, index) {
       });
   }
 });
+
+describe('Implicit Assertions', () => {
+  it('.should() - make an assertion about the current subject', () => {
+    // https://on.cypress.io/should
+    cy.get('.assertion-table')
+      .find('tbody tr:last')
+      .should('have.class', 'success')
+      .find('td')
+      .first()
+      // checking the text of the <td> element in various ways
+      .should('have.text', 'Column content')
+      .should('contain', 'Column content')
+      .should('have.html', 'Column content')
+      // chai-jquery uses "is()" to check if element matches selector
+      .should('match', 'td')
+      // to match text content against a regular expression
+      // first need to invoke jQuery method text()
+      // and then match using regular expression
+      .invoke('text')
+      .should('match', /column content/i)
+
+    // a better way to check element's text content against a regular expression
+    // is to use "cy.contains"
+    // https://on.cypress.io/contains
+    cy.get('.assertion-table')
+      .find('tbody tr:last')
+      // finds first <td> element with text content matching regular expression
+      .contains('td', /column content/i)
+      .should('be.visible')
+
+    // for more information about asserting element's text
+    // see https://on.cypress.io/using-cypress-faq#How-do-I-get-an-element’s-text-contents
+  })
+})
